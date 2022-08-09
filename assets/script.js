0c2c0382b35bbe38a3460bd00f13aa84e39aab69
Scott Rinaberger <scottrinaberger@gmail.com>
2022-08-09 16:47:18 -0500

    implemented timer

--- assets/script.js
+++ assets/script.js
@@ -1,34 +1,34 @@
 // listing core elements
-let highScoreEl = document.getElementById("high_score"); 
+let highScoreEl = document.getElementById("high_score");
 let beginBtnEl = document.getElementById("begin-btn-1");
 
 
 let startBoxEl = document.getElementById("start_quiz");
 let rulesBoxEl = document.getElementById("rules");
 let beginQuizBtnEl = document.getElementById("start_questions");
-let quitQuizBtnEl = document.getElementById("quit_quiz"); 
+let quitQuizBtnEl = document.getElementById("quit_quiz");
 
 let countdownEl = document.getElementById("countdown");
 let countdownTextEl = document.getElementById("countdown_text");
 let quizQuestBoxEl = document.getElementById("q&a");
 
 //variable getting questions and answers
-let questionEl = document.getElementById('question');
-let answerButtonsEl = document.getElementById('answer-btn');
-//var nextQuestionBtnEl = document.getElementById("next_question"); */
+//let questionEl = document.getElementById('question');
+//let answerButtonsEl = document.getElementById('answer-btn');
+let nextQuestionBtnEl = document.getElementById("next_question");
 
 // adding variable text for countdowm
 let messageEl = ('And Here We Go');
 let countdownText = messageEl.split(' ');
 
-//adding variable to shuffle questions
-let shuffledQuestions, currentQuestionIndex;
+//adding timer variable
+let timeCounter = document.getElementById("seconds");
 
 //BEGIN FUNCTIONS
 function seeRules() {
     rulesBoxEl.className += " rules_box_turnOn";
     startBoxEl.className += " start_box_turnOff";
-    highScoreEl.className += " high_score_turnOff";    
+    highScoreEl.className += " high_score_turnOff";
 }
 
 //countdown text function
@@ -37,64 +37,110 @@ function countdownMessage() {
     let wordCount = 0;
     let msgInterval = setInterval(function () {
         if (countdownText[wordCount] === undefined) {
-        beginQuestions()
+            startTimer(60),
+            beginQuestions()
         } else {
-        countdownEl.textContent = countdownText[wordCount];
-        wordCount++;
-        } 
+            countdownEl.textContent = countdownText[wordCount];
+            wordCount++;
+        }
     }, 1000);
 }
 
-function beginQuestions() {  
-    console.log('Started')  
-    //rulesBoxEl.className += " rules_box_turnOff"; 
+function beginQuestions() {
+    console.log('Started')    
     quizQuestBoxEl.className += " start_question_turnOn";
-    //shuffledQuestions = questions.sort(() => Math.random - .5)
-    currentQuestion = 0
-    setNextQuestion()
+    showQuestions(0);    
 }
+
+let que_count = 0;
+let counter;
+//click next to next questions
+nextQuestionBtnEl.onclick = () => {
+    if (que_count < questions.length - 1) {
+        que_count++;
+        showQuestions(que_count);
+    }
+    else {
+        console.log("Questions completed");
+    }
+}
+
+
+function showQuestions(index) {
+    let que_text = document.getElementById("question");
+    let answer_list = document.getElementById("answer-btn");
+    let que_tag = '<span>' + questions[index].question + '</span>';
+    let answer_tag = '<button class="answer_details answer"><span>' + questions[index].options[0] + '</span></button>'
+        + '<button class="answer_details answer"><span>' + questions[index].options[1] + '</span></button>'
+        + '<button class="answer_details answer"><span>' + questions[index].options[2] + '</span></button>'
+        + '<button class="answer_details answer"><span>' + questions[index].options[3] + '</span></button>'
+    que_text.innerHTML = que_tag;
+    answer_list.innerHTML = answer_tag;
+}
+
+function startTimer(time) {
+    counter = setInterval(timer, 1000);
+    function timer() {
+        timeCounter.textContent = time;
+        time--;
+    }
+}
+
 
 function endQuiz() {
     startBoxEl.className += " start_box_turnOn";
     highScoreEl.className += " high_score_turnOn";
-    rulesBoxEl.className += " rules_box_turnOff"; 
-    location.reload()  
+    rulesBoxEl.className += " rules_box_turnOff";
+    location.reload()
 }
 
-
-function setNextQuestion() {
-    console.log('Test 1')
-    //showQuestion(shuffledQuestions[currentQuestionIndex])
-}
-
-function showQuestion(question) {
-    console.log('Test 2')
-    questionEl.innerText = question.question
-}
 
 function selectAnswer() {
 
 }
 
-  
+
 // Start Button Clicks
-beginBtnEl.onclick = seeRules; 
+beginBtnEl.onclick = seeRules;
 
 beginQuizBtnEl.onclick = countdownMessage;
 
 quitQuizBtnEl.onclick = endQuiz;
 
 
+
+
 //adding questions
-const questions = [
+let questions = [
     {
-       question: 'What does JS stand for?', 
-       answers: [
-        { text: 'Java', correct: false },
-        { text: 'Script', correct: false },
-        { text: 'Java Script', correct: true },
-        { text: 'None of the above.', correct: false }
-       ] 
-    }
+        question: 'What does 1 stand for?',
+        answer: "Java Script",
+        options: [
+            "Java",
+            "Script",
+            "Java Script",
+            "None of the above.",
+        ]
+    },
+    {
+        question: 'What does 2 stand for?',
+        answer: "Java Script2",
+        options: [
+            "Java2",
+            "Script2",
+            "Java Script2",
+            "None of the above.2",
+        ]
+    },
+    {
+        question: 'What does 3 stand for?',
+        answer: "Java Script3",
+        options: [
+            "Java3",
+            "Script3",
+            "Java Script3",
+            "None of the above.3",
+        ]
+    },
 ]
 
