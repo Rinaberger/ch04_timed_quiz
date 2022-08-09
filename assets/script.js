927c2aac3c943982d81cbd6fda4fa218fee6b309
Scott Rinaberger <scottrinaberger@gmail.com>
2022-08-08 21:05:35 -0500

    push for josh

--- assets/script.js
+++ assets/script.js
@@ -5,21 +5,38 @@ let beginBtnEl = document.getElementById("begin-btn-1");
 
 let startBoxEl = document.getElementById("start_quiz");
 let rulesBoxEl = document.getElementById("rules");
-//let begin_quiz_btn = rules_box.getElementById("start_quiz");
-//let quit_quiz_btn = rules_box.getElementById("quit_quiz"); 
-
+let beginQuizBtnEl = document.getElementById("start_quiz");
+let quitQuizBtnEl = document.getElementById("quit_quiz"); 
 //add countdown variable
-
-/*var quizQuestionEl = document.getElementById("q&a-event");
-var nextQuestionBtnEl = document.getElementById("next_question"); */
+let quizQuestBoxEl = document.getElementById("q&a");
+//var nextQuestionBtnEl = document.getElementById("next_question"); */
 
 //BEGIN FUNCTIONS
-function startQuiz () {
+function seeRules() {
     rulesBoxEl.className += " rules_box_turnOn";
-    startBoxEl.className += " start_box_dissappear";
-    highScoreEl.className += " high_score_dissappear";
+    startBoxEl.className += " start_box_turnOff";
+    highScoreEl.className += " high_score_turnOff";    
 }
 
+function beginQuestions() {    
+    rulesBoxEl.className += " rules_box_turnOff"; 
+    quizQuestBoxEl.className += " start_question_turnOn";
+}
 
-// Start Button Click
-beginBtnEl.onclick = startQuiz;
+function endQuiz() {
+    startBoxEl.className += " start_box_turnOn";
+    highScoreEl.className += " high_score_turnOn";
+    rulesBoxEl.className += " rules_box_turnOff"; 
+    location.reload()  
+}
+
+  
+// Start Button Clicks
+beginBtnEl.onclick = seeRules; 
+
+beginQuizBtnEl.onclick = beginQuestions;
+
+quitQuizBtnEl.onclick = endQuiz;
+
+
+
